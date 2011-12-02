7810b8b740bcc3e13a42b16c6b288b3acbe1209c
Akrem Osman <akrem1st@hotmail.com>
2011-12-01 22:27:31 -0600

    Akrem Osman

---
+++ TicTacToe/src/edu/luc/tictactoe/businesslogic/implementation/TicTacToeNetworking.java
@@ -0,0 +1,8 @@
+package edu.luc.tictactoe.businesslogic.implementation;
+
+import edu.luc.tictactoe.businesslogic.IPlayer;
+import edu.luc.tictactoe.businesslogic.ITicTacToePlay;
+
+public class TicTacToeNetworking extends TicTacToePlay {
+
+}
