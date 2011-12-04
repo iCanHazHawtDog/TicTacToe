39d32549195dc2aacfa3470942ca526dc9a2cc15
oaatw2004@yahoo.com <omoBABA777>
2011-12-03 20:19:14 -0600

    Selection strats

---
+++ TicTacToe/src/edu/luc/tictactoe/businesslogic/implementation/RandomSelection.java
@@ -0,0 +1,15 @@
+package edu.luc.tictactoe.businesslogic.implementation;
+
+import edu.luc.tictactoe.businesslogic.IBoard;
+import edu.luc.tictactoe.businesslogic.IPair;
+import edu.luc.tictactoe.businesslogic.ISelectionStrategy;
+
+public class RandomSelection implements ISelectionStrategy {
+
+	@Override
+	public IPair<Integer, Integer> execute(IBoard board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
